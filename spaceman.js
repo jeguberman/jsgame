cfb5d53e8eca00ffc96707f6e3a289f919cfad85
John Guberman <jeguberman@gmail.com>
2018-01-23 00:11:42 -0500

    make art robust

--- spaceman.js
+++ spaceman.js
@@ -23,15 +23,62 @@ const falseGame = () => {
   stage.width = Game.WIDTH;
   stage.height = Game.HEIGHT;
 
-  let ctx = stage.getContext('2d');
+  const ctx = stage.getContext('2d');
 
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, stage.width, stage.height);
-  // Draw a non-transformed red rectangle on the left
+  let block = new Image();
+  block.src = '/Users/johnguberman/workspace/App Academy/job_search/jsgame/assets/singleblock.png';
+  const drawPattern = () => {
+    ctx.fillStyle = ctx.createPattern(block, "repeat");
+
+    ctx.fillRect(0, 0, Game.WIDTH, Game.HEIGHT);
+    coin.render();
+  };
+  block.onload = drawPattern;
+
+
+  let coinImage = new Image();
+  coinImage.src = '/Users/johnguberman/workspace/App Academy/job_search/jsgame/assets/coin_test.png';
+
+  function sprite (options) {
+
+    var that = {};
 
 
 
-  stage.addEventListener('mousemove', util.handleMouse);
+    that.context = options.context;
+    that.width = options.width;
+    that.height = options.height;
+    that.image = options.image;
+
+    that.render =  () => {
+      that.context.fillStyle = 'red';
+      // that.context.fillRect(0,0,that.width,that.height);
+      that.context.drawImage(
+        that.image,
+        0,
+        0,
+        that.width,
+        that.height,
+        0,
+        0,
+        that.width,
+        that.height
+      );
+    };
+
+    return that;
+  }
+
+  var coin = sprite({
+    context: ctx,
+    width: 40,
+    height: 40,
+    image: coinImage
+  });
+  // 40,40
+
+
+
 };
 
 
